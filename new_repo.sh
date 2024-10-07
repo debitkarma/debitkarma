dc2cece3fae7ff81cdd345826e510e15eb48ceb8
debitkarma <debitkarma@gmail.com>
2024-10-07 01:08:32 -0400

    Update new_repo.sh
    fixed which argument is passed into the script, added interactive shell hack at the end, and a warning to remember to exit the new shell

--- new_repo.sh
+++ new_repo.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
 
-uv init $0
-cp python.gitignore $0/.gitignore
-cd $0
+uv init $1
+cp python.gitignore $1/.gitignore
+cd $1
 git init .
 git config user.name "Debit Karma"
 git config user.email "debitkarma@gmail.com"
 
 git add pyproject.toml README.md .python-version .gitignore
 git commit -m "initial commit"
+
+gh auth switch -u debitkarma
+echo "DON'T FORGET TO EXIT after creating the repo OR CTRL+C'ing!!"
+bash --init-file <(echo "gh repo create")
